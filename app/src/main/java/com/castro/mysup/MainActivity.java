5e0654edcb0b81c57298e2f139dfe00659f94123
CamilaCA99 <camicastroarratia@gmail.com>
2020-08-31 12:46:13 -0400

    Segundo commit: Se agregan widgets y se generan referencias desde Java

--- app/src/main/java/com/castro/mysup/MainActivity.java
+++ app/src/main/java/com/castro/mysup/MainActivity.java
@@ -2,13 +2,29 @@ package com.castro.mysup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Creamos las variables que harán referencia a los widgets
+    private TextView txtTitulo;
+    private Button btnIniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Realizar las referencias
+        txtTitulo = findViewById(R.id.txtTitulo);
+        btnIniciar = findViewById(R.id.btnIniciar);
+
+        txtTitulo.setTextColor(Color.BLUE);
+    }
+
+    public void Saludar(){
+
     }
 }
