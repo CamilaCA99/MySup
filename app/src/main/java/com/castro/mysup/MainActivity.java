0eb8fb1e61a316e835a0896d91d736c10309a92d
CamilaCA99 <camicastroarratia@gmail.com>
2020-12-07 17:02:35 -0300

    Primeros métodos Onclick

--- app/src/main/java/com/castro/mysup/MainActivity.java
+++ app/src/main/java/com/castro/mysup/MainActivity.java
@@ -1,35 +1,58 @@
 package com.castro.mysup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    //Creamos las variables que harán referencia a los widgets
-    private TextView txtTitulo;
-    private Button btnIniciar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText txtEmail, txtPassword;
+    Button btnIniciar, btnRegistrar;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Realizar las referencias
-        txtTitulo = findViewById(R.id.txtTitulo);
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPassword = findViewById(R.id.txtPassword);
         btnIniciar = findViewById(R.id.btnIniciar);
+        btnRegistrar = findViewById(R.id.btnRegistrar);
 
-        txtTitulo.setTextColor(Color.BLUE);
+        btnIniciar.setOnClickListener(this);
+        btnRegistrar.setOnClickListener(this);
     }
 
-    public void Saludar(View view){
-        Toast.makeText(this,"hola mi pana banana",Toast.LENGTH_LONG).show();
-    }
-    public void Despedir(View bai) {
-        Toast.makeText(this, "chao mi pana banana", Toast.LENGTH_LONG).show();
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnIniciar:
+
+                break;
+            case R.id.btnRegistrar:
+                
+                break;
+        }
     }
 }
